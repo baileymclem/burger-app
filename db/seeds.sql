7fe2f15696396da28debecf20fcdf9834255aa99
Bailey Clemons <baileythevampireslayer@gmail.com>
2021-01-17 16:49:40 -0800

    "set-up schema and controllers"

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
