0fe23272c24045d29ada3d2edb99ec005b222d58
Bailey Clemons <baileythevampireslayer@gmail.com>
2021-02-26 02:11:27 -0800

    views

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8080,
   user: "root",
-  password: process.env.MYSQL_PASSWORD,
+  password: "WYNN!373sql",
   database: "burgers_db"
 });
 
